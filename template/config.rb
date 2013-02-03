f59f947d79726b5755e40e0ffc1a01f4dab2aeba
Yorick Peterse <yorickpeterse@gmail.com>
2013-02-03 21:44:50 +0100

    Updated the MANIFEST and default config file.
    Signed-off-by: Yorick Peterse <yorickpeterse@gmail.com>

--- template/config.rb
+++ template/config.rb
@@ -1,6 +1,6 @@
 # This is the main configuration file for your RubyGems mirror. Here you can
-# change settings such as the location to store Gem files in, what Gems to
-# ignore, what sources to mirror and so on.
+# change settings such as the location to store Gem files in and what sources
+# and Gems you'd like to mirror.
 GemMirror.configuration.configure do
   # The directory to store indexing information as well as the Gem files in.
   destination File.expand_path('../public', __FILE__)
@@ -11,8 +11,10 @@ GemMirror.configuration.configure do
   # When set to `true` development dependencies of Gems will also be mirrored.
   development false
 
-  # By default everything is logged.
-  # logger.level = Logger::ERROR
+  # If you're mirroring a lot of Gems you'll probably want to switch the
+  # logging level to Logger::ERROR or Logger::INFO to reduce the amount of
+  # noise.
+  logger.level = Logger::DEBUG
 
   # A source is a remote location that you want to mirror. The first parameter
   # of this method is the human readable name, the second one the URL. The
